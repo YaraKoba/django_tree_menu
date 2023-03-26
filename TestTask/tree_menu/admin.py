a3f4df74677459bfe3456c9ff947f0aa5cc10e41
yarakoba <zangetsyyy@mail.ru>
2023-03-26 16:15:31 +0400

    second commit

--- TestTask/tree_menu/admin.py
+++ TestTask/tree_menu/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from tree_menu.models import MenuGroup, MenuItem
 
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('text', 'group', 'parent', 'get_children')
+
+
+admin.site.register(MenuItem, MenuItemAdmin)
+
 admin.site.register(MenuGroup)
 
-admin.site.register(MenuItem)
--- TestTask/tree_menu/models.py
+++ TestTask/tree_menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.dispatch import receiver
 from django.urls import reverse
 
 
@@ -11,7 +12,7 @@ class MenuGroup(models.Model):
 
 class MenuItem(models.Model):
     text = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=55, unique=True, db_index=True, verbose_name='URL')
+    # slug = models.SlugField(max_length=55, unique=True, db_index=True, verbose_name='URL')
     parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='children')
     url = models.CharField(max_length=100)
     group = models.ForeignKey(MenuGroup, related_name='menuitem', on_delete=models.CASCADE)
@@ -19,8 +20,17 @@ class MenuItem(models.Model):
     def __str__(self):
         return f'text: {self.text}, group: {self.group}'
 
+    def get_children(self):
+        return ", ".join([child.text for child in self.children.all()])
+
+    get_children.short_description = "Children"
+
+    class Meta:
+        verbose_name_plural = "Menu Items"
+
     def get_url(self):
         return reverse('menu', kwargs={'menu_id': self.group.pk, 'menu_item_id': self.pk})
+
 
 
 
